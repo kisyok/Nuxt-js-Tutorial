638d87d4e761e40f76884ead3211e7d1a80b23bb
Kisyok <kisoindran@gmail.com>
2022-02-14 16:15:41 +0800

    ip

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <Tutorial />
+  <div>
+    <nav-item />
+    <div class="container">
+      <hero-item />
+      <large-card-display
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+      <small-card-display
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import { largeCardSections, smallCardSections } from '../assets/data.js'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    }
+  }
 }
 </script>
---
+++ pages/my-items.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    my items
+  </h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
